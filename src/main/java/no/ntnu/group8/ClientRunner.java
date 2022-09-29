dfe7c7a9015404e9b2a08e40418e95bfe5f3380f
z-rayc <zilerayser@gmail.com>
2022-09-29 13:14:13 +0200

    Remove unused print

--- src/main/java/no/ntnu/group8/ClientRunner.java
+++ src/main/java/no/ntnu/group8/ClientRunner.java
@@ -6,8 +6,6 @@ public class ClientRunner {
     public ClientRunner() {
         try {
             Client client = new Client("129.241.152.12", 1234);
-    
-            System.err.println("Write a message to the server: ");
         } catch (SocketException e) {
             System.err.println("Error: Socket not created correctly");
         }
