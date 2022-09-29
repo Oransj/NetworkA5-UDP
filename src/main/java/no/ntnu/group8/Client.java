92a10a3d7d7b178017a12f9efb7810226631f70a
Oransj <98455426+Oransj@users.noreply.github.com>
2022-09-29 11:46:10 +0200

    Create sendMessage and constructor

--- src/main/java/no/ntnu/group8/Client.java
+++ src/main/java/no/ntnu/group8/Client.java
@@ -1,4 +1,26 @@
 package no.ntnu.group8;
 
+import java.io.IOException;
+import java.net.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
 public class Client {
+    private final DatagramSocket clientSocket;
+    private final String address = "129.241.152.12";
+    private final int port = 1234;
+
+    public Client() throws SocketException {
+        clientSocket = new DatagramSocket();
+    }
+
+    public String sendMessage(String message) throws IOException {
+        byte[] messageByte = message.getBytes(StandardCharsets.UTF_8);
+        DatagramPacket packet = new DatagramPacket(messageByte, messageByte.length, InetAddress.getByName(address), port);
+        clientSocket.send(packet);
+        packet = new DatagramPacket(messageByte, messageByte.length);
+        clientSocket.receive(packet);
+        return Arrays.toString(packet.getData());
+    }
+
 }
