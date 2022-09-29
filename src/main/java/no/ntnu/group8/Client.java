6e56614fb8d9d5cf17bc74d38c470d039066e1f9
Oransj <98455426+Oransj@users.noreply.github.com>
2022-09-29 12:50:02 +0200

    Add client documentation

--- src/main/java/no/ntnu/group8/Client.java
+++ src/main/java/no/ntnu/group8/Client.java
@@ -10,6 +10,14 @@ public class Client {
     private final String address;
     private final int port;
 
+    /**
+     *
+     * This is the client which communicates with the server.
+     *
+     * @param address address to
+     * @param port
+     * @throws SocketException
+     */
     public Client(String address, int port) throws SocketException {
         this.address = address;
         this.port = port;
@@ -19,10 +27,15 @@ public class Client {
     public String sendMessage(String message) throws IOException {
         byte[] messageByte = message.getBytes(StandardCharsets.UTF_8);
         DatagramPacket packet = new DatagramPacket(messageByte, messageByte.length, InetAddress.getByName(address), port);
+        System.err.println("DatagramPacket successfully created");
         clientSocket.send(packet);
+        System.err.println("Packet successfully sent");
         packet = new DatagramPacket(messageByte, messageByte.length);
         clientSocket.receive(packet);
-        return Arrays.toString(packet.getData());
+        System.err.println("Packet successfully received");
+        System.err.println("Packet data: " + Arrays.toString(packet.getData()));
+        String returnValue = new String(packet.getData(), 0, packet.getLength());
+        return returnValue;
     }
 
 }
