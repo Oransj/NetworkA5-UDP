8bbe95c5a0f0c841ae1b9e237020cc8acc5ac828
z-rayc <zilerayser@gmail.com>
2022-09-29 11:54:20 +0200

    Add parameters to Client constructor

--- src/main/java/no/ntnu/group8/ClientRunner.java
+++ src/main/java/no/ntnu/group8/ClientRunner.java
@@ -5,7 +5,7 @@ import java.net.SocketException;
 public class ClientRunner {
     public ClientRunner() {
         try {
-            Client client = new Client();
+            Client client = new Client("129.241.152.12", 1234);
         } catch (SocketException e) {
             System.out.println("Error: Socket not created correctly");
         }
